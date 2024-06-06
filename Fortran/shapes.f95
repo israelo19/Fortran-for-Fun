e04b8497e64bc63c8465d70b7e15dbe608127625
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-06-06 14:44:31 -0400

    Working CLI version

--- Fortran/shapes.f95
+++ Fortran/shapes.f95
@@ -6,22 +6,41 @@ program shapes
 implicit none
 
 real :: area, volume, height, circumfrence, diameter, radius, pi, length, width !variables needed
-
 character :: needs
 print *, "Welcome to the Shape Calculator! This program will help you calculate the dimentions of a shape! Press Enter to conntiue!"
 read (*,*)
-print *, "First thing's first!(Enter)"
+print *, "First thing's first!(enter)"
 read (*,*)
-print *, "Is your shape contain a circle? (Y/N)"
+print *, "If your shape is a sphere hit 'Y', if it's a rectangular prism or cube, hit 'N'"
 read *, needs
 
 if(needs == 'Y' .or. needs == 'y') then
     pi = 3.14159
 
-    print *, "enter length, width, and radius(in this order)"
-    read *, length, width, radius
-    print *, "the length width and radius are: ", length, width, radius
+    print *, "enter Radius: "
+    read *, radius
+   ! print *, "the length width and radius are: ", length, width, radius
 
+    ! Perform calculations
+
+    diameter = 2 * radius
+    area = 4 * pi * radius * radius
+    circumfrence = 2 * pi * radius
+    
+    print *, "Your dimensions are: Area: ", area, "Circumfrence: ", circumfrence, "Diameter: ", diameter
+    else
+        print *, "Enter length, width and height(in this order)"
+        read *, length, width, height
+
+        !Perform calculations
+
+       ! area = length * width
+        volume = length * width * height
+        
+        print *, "Your dimensions are: Volume: ", volume
+    
+
+        !print *, "the length and width are: ", length, width
 end if
 
 
@@ -31,26 +50,3 @@ end if
 
 
 end program shapes
-
-
-
-
-
-
-
-
-
-!read *, needs_pi
-
-!if(needs_pi .eqv. .true.) then
- !   print *, "Pi is true"
-!end if
-
-
-!if(needs_pi .eqv. .true.) then
- !   print *, "Pi is true"
-!end if
-
-!if(p .eqv. .true.)
- !   print *, "YOU NEED PI"
-!end if
