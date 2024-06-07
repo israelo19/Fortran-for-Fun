eb96106ebd7dc74f887dd7c4ff52a25fe241c249
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-06-07 11:48:08 -0400

    Fixed the loops and writing
    This change uses iostat to read file and

--- Fortran/shapes.f95
+++ Fortran/shapes.f95
@@ -7,7 +7,7 @@ implicit none
 
 !variables needed
 real :: area, volume, height, circumfrence, diameter, radius, pi, length, width 
-integer :: i 
+integer :: i, ierr
 character :: needs
 
 !for loops below
@@ -19,32 +19,44 @@ read (*,*)
 print *, "Is your shape a sphere? (Y/N)"
 read (*,*) needs
 
-!If the shape is a sphere then instantiate pi, read the sphere data file and perform the calculations.
-if(needs == 'Y' .or. needs == 'y') then
-    pi = 3.14159
-    write(*,*) "Reading Radius: "
-    open(unit = 10, file = "sphere.txt")
-    do while(.true.)
-        read (5, *, end=100) radius
-        ! Perform calculations
-        diameter = 2 * radius
-        area = 4 * pi * radius * radius
-        circumfrence = 2 * pi * radius
-        print *, "Your dimensions are: Area: ", area, "Circumfrence: ", circumfrence, "Diameter: ", diameter
-    end do
-    100 continue
-    close(10)    
-else
-    !If the shape is not a sphere(cube), read the cube data file and perform calcuations.
-    open(unit = 10, file = "cube.txt")
-    do while(.true.)
-        read (10, *, end=200) length, width, height
-        !Perform calculations
-        volume = length * width * height
-        print *, "Your dimensions are: Volume: ", volume
-    end do
-    200 continue
-    close(10)
-end if
-
+    !If the shape is a sphere then instantiate pi, read the sphere data file and perform the calculations.
+    if(needs == 'Y' .or. needs == 'y') then
+        pi = 3.14159
+        write(*,*) "Reading Radius: "
+        open(unit = 10, file = "sphere.txt")
+            do 
+                read(10, *, iostat = ierr) radius
+                if(ierr > 0) then
+                    print *, "Error reading file"
+                    exit
+                else if(ierr < 0) then
+                    print *, "End of file reached"
+                    exit
+                else
+                    ! Perform calculations
+                    diameter = 2 * radius
+                    area = 4 * pi * radius * radius
+                    circumfrence = 2 * pi * radius
+                    print *, "Your dimensions are: Area: ", area, "Circumfrence: ", circumfrence, "Diameter: ", diameter
+                end if
+            end do
+        close(10)    
+    else
+        !If the shape is not a sphere(cube), read the cube data file and perform calcuations.
+        open(unit = 10, file = "cube.txt")
+        do 
+            read(10, *, iostat = ierr) length, width, height
+            if(ierr > 0) then
+                print *, "Error reading file"
+                exit
+            else if(ierr < 0) then
+                print *, "End of file reached"
+                exit
+            else
+                ! Perform calculations
+                volume = length * width * height
+                print *, "Your volume is: ", volume
+            end if
+        end do
+    end if
 end program shapes
