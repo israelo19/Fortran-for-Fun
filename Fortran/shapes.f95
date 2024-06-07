7a7762840cc9a4cbf0f4c00d83afa39f68e4511e
iogwu-admin <iogwu@sspa.com>
2024-06-07 09:55:24 -0400

    Comments

--- Fortran/shapes.f95
+++ Fortran/shapes.f95
@@ -1,14 +1,16 @@
 ! This is a program that will display the dimentions of a shape
 ! It will display the area, volume, height, circumfrence, diameter and radius(if applicable)
 ! Israel Ogwu 05/25/2024
-! NGA
+
 program shapes
 implicit none
 
-real :: area, volume, height, circumfrence, diameter, radius, pi, length, width !variables needed
+!variables needed
+real :: area, volume, height, circumfrence, diameter, radius, pi, length, width 
 integer :: i 
 character :: needs
 
+!for loops below
 i = 1
 
 write(*,*) "Welcome to the Shape Calculator! This program will help you calculate the dimentions of a shape! Press Enter to conntiue!"    
@@ -17,10 +19,11 @@ read (*,*)
 print *, "Is your shape a sphere? (Y/N)"
 read (*,*) needs
 
+!If the shape is a sphere then instantiate pi, read the sphere data file and perform the calculations.
 if(needs == 'Y' .or. needs == 'y') then
     pi = 3.14159
     write(*,*) "Reading Radius: "
-    open(unit = 5, file = "sphere.txt")
+    open(unit = 10, file = "sphere.txt")
     do while(.true.)
         read (5, *, end=100) radius
         ! Perform calculations
@@ -30,22 +33,18 @@ if(needs == 'Y' .or. needs == 'y') then
         print *, "Your dimensions are: Area: ", area, "Circumfrence: ", circumfrence, "Diameter: ", diameter
     end do
     100 continue
-    close(5)
+    close(10)    
 else
+    !If the shape is not a sphere(cube), read the cube data file and perform calcuations.
     open(unit = 10, file = "cube.txt")
     do while(.true.)
         read (10, *, end=200) length, width, height
-  
-
         !Perform calculations
-
         volume = length * width * height
-            
         print *, "Your dimensions are: Volume: ", volume
     end do
     200 continue
     close(10)
-
 end if
 
 end program shapes
