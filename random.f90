4d7720c299f0b7035b074d4b59ccb19ae7ef369f
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-06-17 11:26:20 -0400

    added random number generator
    writes to dat file and uses gnuplot to create graph

--- random.f90
+++ random.f90
@@ -4,18 +4,18 @@ implicit none
 ! this is with the random_number function
 
 ! real :: r
-integer :: i, n, count, upper_lim = 100, lower_lim = -100
-count = 0
-n=10
-do i = 1, n
-call random_number(r)
-    r = lower_lim + (upper_lim - lower_lim)*r
-    write(*,*) "Random number: " , r
-    count = count + 1
+! integer :: i, n, count, upper_lim = 100, lower_lim = -100
+! count = 0
+! n=10
+! do i = 1, n
+! call random_number(r)
+!     r = lower_lim + (upper_lim - lower_lim)*r
+!     write(*,*) "Random number: " , r
+!     count = count + 1
 
-end do
-    write(*,*) "Count: " , count
-end program random
+! end do
+!     write(*,*) "Count: " , count
+! end program random
 
 
 
@@ -24,15 +24,16 @@ end program random
 real :: x, y
 integer :: i, n
 integer :: seed
+integer :: upper_lim = 100, lower_lim = -100
 seed = 123456789
 
 call srand(seed)
 
-
+open (unit=10, file='random.dat', status='unknown')
 do i = 1, 100
     x = lower_lim + (upper_lim-lower_lim)*rand()
-    y = lower_lim + (upper_lim-lower_lim)*rand()
-    write(*,*)"Random x: ", x , "Random y: ", y
+  !  y = lower_lim + (upper_lim-lower_lim)*rand()
+    write(10,*)x
 end do 
 
 end program random
